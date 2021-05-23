3a886fa79b41a2e859bfcb5d0bf1ed2656242c94
LeonardoMFSilva <leo_leb5@hotmail.com>
2021-05-23 15:40:42 -0300

    Creating new tests and methods

--- src/patientintake/ClinicMain.java
+++ src/patientintake/ClinicMain.java
@@ -1,5 +1,6 @@
 package patientintake;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
@@ -8,7 +9,7 @@ public class ClinicMain {
     private static ClinicCalendar calendar;
 
     public static void main(String[] args) throws Throwable {
-        calendar = new ClinicCalendar();
+        calendar = new ClinicCalendar(LocalDate.now());
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Patient Intake Computer System!\n\n");
         String lastOption = "";
@@ -22,6 +23,7 @@ public class ClinicMain {
         System.out.println("Please select an option:");
         System.out.println("1. Enter a Patient Appointment");
         System.out.println("2. View All Appointments");
+        System.out.println("3. View Today's Appointments");
         System.out.println("X. Exit System.");
         System.out.println("Option: ");
         String option = scanner.next();
--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -2,6 +2,7 @@ package patientintake;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -11,7 +12,7 @@ class ClinicCalendarShould {
 
     @Test
      void allowEntryOfAnAppointment(){
-        ClinicCalendar calendar = new ClinicCalendar();
+        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
         calendar.addAppointment("Jim", "Weaver", "avery", "09/01/2018 2:00 pm");
         List<PatientAppointment> appointments = calendar.getAppointments();
         assertNotNull(appointments);
@@ -19,8 +20,29 @@ class ClinicCalendarShould {
         PatientAppointment enteredAppt = appointments.get(0);
         assertEquals("Jim", enteredAppt.getPatientFirstName());
         assertEquals("Weaver", enteredAppt.getPatientLastName());
-        assertEquals(Doctor.avery, enteredAppt.getDoctor());// can also be used assertSame
+        assertEquals(Doctor.avery, enteredAppt.getDoctor());// can also be used assertSame, that objects need to be exactly the same - assertNotSame, reversal, verify object isn't the same instance as the original
         assertEquals("9/1/2018 02:00 PM", enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a")));
     }
 
+    @Test
+    void returnTrueForHasAppointmentsIfThereAreAppointments(){
+        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+        calendar.addAppointment("Jim", "Weaver", "avery", "09/01/2018 2:00 pm");
+        assertTrue(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+    }
+
+    @Test
+    void returnFalseForHasAppointmentsIfThereAreNoAppointments(){
+        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+        assertFalse(calendar.hasAppointment(LocalDate.of(2018, 9, 1)));
+    }
+
+    @Test
+    void returnCurrentDaysAppointment(){
+        ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+        calendar.addAppointment("Jim", "Weaver", "avery", "08/26/2018 2:00 pm");
+        calendar.addAppointment("Jim", "Weaver", "avery", "08/26/2018 2:00 pm");
+        calendar.addAppointment("Jim", "Weaver", "avery", "9/01/2018 2:00 pm");
+        assertEquals(2, calendar.getAppointments());
+    }
 }
